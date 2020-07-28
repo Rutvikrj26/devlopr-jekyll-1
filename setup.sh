8ebef3d89a5695d48db9b909917b3f2a567cb078
rutvikrj <rutvikrj26@gmail.com>
2020-07-28 18:23:29 +0530

    Direct Setup

--- setup.sh
+++ setup.sh
@@ -2,5 +2,6 @@ git pull
 git add -A 
 git commit -m "Direct Setup"
 git push origin master
+ALGOLIA_API_KEY='e0bd1592fe70406a844d4ff49146bc20' bundle exec jekyll algolia
 set ALGOLIA_API_KEY='e0bd1592fe70406a844d4ff49146bc20'
 jekyll algolia ALGOLIA_API_KEY
